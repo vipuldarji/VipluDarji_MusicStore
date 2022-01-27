9f11b617f40883d6ce8cff4e6869f6a10e110b76
Vipul <vipulchetandarji@gmail.com>
2022-01-27 16:15:46 -0500

    Assignment-1

---
+++ APP_SERVER/controllers/list.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const books = [
+    { title: 'Subtle Things', author: 'Marian Hill', src: "/images/Subtle Things.jpg" },
+    { title: 'DIGGYDOWN', author: 'Marian Hill', src: "/images/Diggydown.jpg" },
+    { title: 'Red Lips', author: 'Camila Cabello', src: "/images/Havana.jpg" },
+];
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('list-display', { title: 'Books', books });
+});
+
+module.exports = router;
---
+++ APP_SERVER/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+const listRouter = require('../controllers/list');
+const aboutRouter = require('../controllers/about');
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'The Music Store' });
+});
+
+router.use('/about', aboutRouter);
+
+router.use('/list', listRouter);
+
+router.get('/display', function (req, res, next) {
+    res.render('display', { title: 'Display' });
+})
+
+module.exports = router;
